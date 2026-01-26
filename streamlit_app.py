41b96226935596b440740aa3ab71397fa19be9c9
packages.txt <leehunna789@gmail.com>
2026-01-26 09:20:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,7 @@ st.markdown("""
     .stButton>button { 
      stAppbackground-color: #FF0000; color: white; border-radius: 10px; 
      height: 60px; font-weight: bold; border: 2px solid #FFD700; }
+    
     </style>
     """, unsafe_allow_html=True)
 
